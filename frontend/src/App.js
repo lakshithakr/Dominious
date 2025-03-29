5eeb18f9e1ec63ccd06bd4f580661d4817bf63ec
lakshithakr <lakshithakumuduranga102@gmail.com>
2025-03-29 14:30:57 +0530

    about_and_description

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
+import About from './Components/About/About';
+import Description from './Components/Description/Description';
 function App() {
   return (
     <>
     <NavBar/>
+    <About/>
+    <Description/>
     <Footer/>
     </>
   );
