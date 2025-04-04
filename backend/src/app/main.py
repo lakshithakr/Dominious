f3ea8597bb5c03a5e5de8ca5391cfd058c3d3adc
lakshithakr <lakshithakumuduranga102@gmail.com>
2025-04-04 23:25:53 +0530

    backend

---
+++ backend/src/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
+app= FastAPI()
+
+origins=["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ backend/src/utills.py
@@ -0,0 +1,11 @@
+def preprocess():
+    pass
+
+def generate_domains():
+    pass
+def postprocessing():
+    pass
+def final_domains():
+    pass
+def domain_details():
+    pass
